2021e2412937bdc0e5c05a25548ccbb9dc5d26b6
Fernando Isidro Luna <luffynando@gmail.com>
2024-12-07 15:23:37 -0600

    refactor: :recycle: remove unnused prettier config

--- src/adonis.ts
+++
@@ -1,9 +0,0 @@
-import { type Config } from "prettier";
-import configBase from "./base.js";
-
-const configAdonis: Config = {
-  ...configBase,
-  plugins: ["prettier-edgejs"],
-};
-
-export default configAdonis;
